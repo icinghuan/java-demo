1129559ce0f6b7e71e45df83657ac31ea3f0aa47
xuyuan <xuyuan@tigerbrokers.com>
2018-10-26 16:06:48 +0800

    Sudoku

--- src/main/java/top/icinghuan/demo/javatest2/Sudoku.java
+++ src/main/java/top/icinghuan/demo/javatest2/Sudoku.java
@@ -30,58 +30,74 @@ public class Sudoku extends JFrame implements ActionListener {
             .setNameFormat("demo-pool-%d").build();
     private static ExecutorService singleThreadPool = new ThreadPoolExecutor(16, 32,
             0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
-    private Thread thread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            getSudoku();
-        }
-    });
     private JTextPane[][] jTextPanes = new JTextPane[9][9];
     private JPanel[] jPanels = new JPanel[9];
     private JMenu[] jMenus;
     private JMenuItem[] jMenuItems;
+    private SimpleAttributeSet set1, set2, set3;
 
     private Integer[][] gameData = new Integer[9][9];
+    private boolean[][] gameFlags = new boolean[9][9];
 
     DocumentListener documentListener = new DocumentListener() {
         @Override
         public void insertUpdate(DocumentEvent e) {
-            singleThreadPool.execute(thread);
+//            threadRun();
         }
 
         @Override
         public void removeUpdate(DocumentEvent e) {
-            singleThreadPool.execute(thread);
+//            threadRun();
         }
 
         @Override
         public void changedUpdate(DocumentEvent e) {
-            singleThreadPool.execute(thread);
+//            threadRun();
         }
     };
 
+    private void threadRun() {
+        singleThreadPool.execute(() -> {
+            getSudoku();
+            showSudoku();
+        });
+    }
+
     public Sudoku() {
+        set1 = new SimpleAttributeSet();
+        StyleConstants.setForeground(set1, Color.BLACK);
+        StyleConstants.setAlignment(set1, StyleConstants.ALIGN_CENTER);
+        StyleConstants.setBold(set1, true);
+        StyleConstants.setFontFamily(set1, "Times New Roman");
+        StyleConstants.setFontSize(set1, 32);
+
+        set2 = new SimpleAttributeSet();
+        StyleConstants.setForeground(set2, Color.GREEN);
+        StyleConstants.setAlignment(set2, StyleConstants.ALIGN_CENTER);
+        StyleConstants.setFontFamily(set2, "Times New Roman");
+        StyleConstants.setFontSize(set2, 48);
+
+        set3 = new SimpleAttributeSet();
+        StyleConstants.setForeground(set3, Color.RED);
+        StyleConstants.setAlignment(set3, StyleConstants.ALIGN_CENTER);
+        StyleConstants.setFontFamily(set3, "Times New Roman");
+        StyleConstants.setFontSize(set3, 48);
+
         for (int i = 0; i < 9; i++) {
             jPanels[i] = new JPanel();
             jPanels[i].setLayout(new GridLayout(3, 3, 2, 2));
             jPanels[i].setSize(150, 150);
         }
-        SimpleAttributeSet set1 = new SimpleAttributeSet();
-        StyleConstants.setForeground(set1, Color.BLACK);
-        StyleConstants.setFontFamily(set1, "Times New Roman");
-        StyleConstants.setFontSize(set1, 32);
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 gameData[i][j] = 0;
                 jTextPanes[i][j] = new JTextPane();
                 jTextPanes[i][j].setText("");
-                StyledDocument styledDocument = jTextPanes[i][j].getStyledDocument();
-                styledDocument.setCharacterAttributes(0, 1, set1, false);
                 jTextPanes[i][j].setEditable(false);
                 jTextPanes[i][j].setSize(50, 50);
                 jTextPanes[i][j].getDocument().addDocumentListener(documentListener);
-                jPanels[i].add(jTextPanes[i][j], BorderLayout.CENTER);
+                jPanels[i / 3 * 3 + j / 3].add(jTextPanes[i][j]);
             }
         }
         JPanel jPanel = new JPanel();
@@ -132,7 +148,7 @@ public class Sudoku extends JFrame implements ActionListener {
             newSudoku();
         }
         if (e.getSource() == jMenuItems[1]) {
-            if (!checkSudoku()) {
+            if (!checkSudoku(false)) {
                 JOptionPane.showMessageDialog(null, "Can not save the game!", "Save Game", JOptionPane.PLAIN_MESSAGE);
                 return;
             }
@@ -142,7 +158,7 @@ public class Sudoku extends JFrame implements ActionListener {
         }
         if (e.getSource() == jMenuItems[2]) {
             // TODO Load Game
-            if (!checkSudoku()) {
+            if (!checkSudoku(false)) {
                 JOptionPane.showMessageDialog(null, "Can not load the game!", "Load Game", JOptionPane.PLAIN_MESSAGE);
                 return;
             }
@@ -162,7 +178,7 @@ public class Sudoku extends JFrame implements ActionListener {
 
     }
 
-    private boolean checkSudoku() {
+    private boolean checkSudoku(boolean update) {
         boolean flag = true;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -178,32 +194,22 @@ public class Sudoku extends JFrame implements ActionListener {
                 flags[j] = false;
             }
             for (int j = 0; j < 9; j++) {
-                if (gameData[i][j] != 0 && flags[gameData[i][j]]) {
-                    gameData[i][j] = 0;
-                    flag = false;
-                } else {
-                    flags[gameData[i][j]] = true;
-                }
+                evaluate(i, j, flags, update);
             }
             for (int j = 0; j < 10; j++) {
                 flags[j] = false;
             }
             for (int j = 0; j < 9; j++) {
-                if (gameData[j][i] != 0 && flags[gameData[j][i]]) {
-                    gameData[i][j] = 0;
-                    flag = false;
-                } else {
-                    flags[gameData[j][i]] = true;
-                }
+                evaluate(j, i, flags, update);
             }
         }
-        if (!checkSudokuNineGrids()) {
+        if (!checkSudokuNineGrids(update)) {
             flag = false;
         }
         return flag;
     }
 
-    private boolean checkSudokuNineGrids() {
+    private boolean checkSudokuNineGrids(boolean update) {
         boolean flag = true;
         for (int k = 0; k < 9; k++) {
             for (int i = (k * 3) % 9; i < (k * 3) % 9 + 3; i++) {
@@ -212,31 +218,65 @@ public class Sudoku extends JFrame implements ActionListener {
                     flags[j] = false;
                 }
                 for (int j = (k / 3) * 3; j < (k / 3) * 3 + 3; j++) {
-                    if (gameData[i][j] != 0 && flags[gameData[i][j]]) {
-                        gameData[i][j] = 0;
-                        flag = false;
-                    } else {
-                        flags[gameData[i][j]] = true;
-                    }
+                    evaluate(i, j, flags, update);
                 }
             }
         }
         return flag;
     }
 
+    private boolean evaluate(int i, int j, boolean[] flags, boolean update) {
+        boolean flag = true;
+        if (gameData[i][j] == 0) {
+            gameFlags[i][j] = false;
+        } else {
+            if (flags[gameData[i][j]]) {
+                if (update) {
+                    gameData[i][j] = 0;
+                }
+                gameFlags[i][j] = false;
+                flag = false;
+            } else {
+                gameFlags[i][j] = true;
+                flags[gameData[i][j]] = true;
+            }
+        }
+        return flag;
+    }
+
     private void showSudoku() {
-        checkSudoku();
+        checkSudoku(false);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (NUM_SET.contains(gameData[i][j])) {
                     if (gameData[i][j] == 0) {
                         jTextPanes[i][j].setText("");
+                        jTextPanes[i][j].repaint();
+                        continue;
                     } else {
                         jTextPanes[i][j].setText(gameData[i][j].toString());
+                    }
+                    StyledDocument styledDocument = jTextPanes[i][j].getStyledDocument();
+                    if (jTextPanes[i][j].isEditable()) {
+                        if (gameFlags[i][j]) {
+                            styledDocument.setCharacterAttributes(0, 1, set2, false);
+                        } else {
+                            styledDocument.setCharacterAttributes(0, 1, set3, false);
+                        }
+                    } else {
+                        styledDocument.setCharacterAttributes(0, 1, set1, false);
                     }
                     jTextPanes[i][j].repaint();
                 }
             }
+        }
+        if (finishGame()) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    jTextPanes[i][j].setEditable(false);
+                }
+            }
+            JOptionPane.showMessageDialog(null, "You have finished the game!", "Congratulations", JOptionPane.PLAIN_MESSAGE);
         }
     }
 
@@ -254,42 +294,93 @@ public class Sudoku extends JFrame implements ActionListener {
                 }
             }
         }
-        showSudoku();
-        if (finishGame()) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    jTextPanes[i][j].setEditable(false);
-                }
-            }
-            JOptionPane.showMessageDialog(null, "You have finished the game!", "Congratulations", JOptionPane.PLAIN_MESSAGE);
-        }
     }
 
     private void newSudoku() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                jTextPanes[i][j].setEditable(false);
+            }
+        }
         Random random = new Random();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 gameData[i][j] = random.nextInt(10);
+                gameFlags[i][j] = false;
             }
         }
+        checkSudoku(true);
         showSudoku();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                jTextPanes[i][j].setEditable(true);
+                if (gameData[i][j] == 0) {
+                    jTextPanes[i][j].setEditable(true);
+                }
             }
         }
+        singleThreadPool.execute(() -> {
+            while (!finishGame()) {
+                getSudoku();
+                showSudoku();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private boolean finishGame() {
-        checkSudoku();
+        checkSudoku(false);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (gameData[i][j] == 0) {
+                if (!gameFlags[i][j]) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private String gameToString() {
+        checkSudoku(true);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                stringBuilder.append(gameData[i][j]);
+            }
+        }
+        String str = stringBuilder.toString();
+        return encrypt(str);
+    }
+
+    private void loadFromData(String str) {
+        String s = decrypt(str);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                gameData[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            try {
+                gameData[i / 9][i % 9] = Integer.parseInt(ch.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        checkSudoku(true);
+    }
+
+    private String encrypt(String str) {
+        // TODO encrypt
+        return str;
+    }
+
+    private String decrypt(String str) {
+        // TODO decrypt
+        return str;
     }
 
     public static void main(String[] args) {
